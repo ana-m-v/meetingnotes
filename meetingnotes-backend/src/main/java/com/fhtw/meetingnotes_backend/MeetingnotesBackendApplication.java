481d2640309b2005aafa32bc0e6a4cf04b1af7e1
ana-m-v <114224301+ana-m-v@users.noreply.github.com>
2026-03-02 20:25:30 +0100

    initial commit

---
+++ meetingnotes-backend/src/main/java/com/fhtw/meetingnotes_backend/MeetingnotesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fhtw.meetingnotes_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingnotesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingnotesBackendApplication.class, args);
+	}
+
+}
---
+++ meetingnotes-backend/src/test/java/com/fhtw/meetingnotes_backend/MeetingnotesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhtw.meetingnotes_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingnotesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
